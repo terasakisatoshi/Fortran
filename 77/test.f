5ea8c59ee7a0dec699575a638ca55991b5f91e3b
terasakisatoshi <terasakisatoshi.math@gmail.com>
2016-09-02 13:50:59 +0900

    add test.f to check compile test

---
+++ 77/test.f
@@ -0,0 +1,11 @@
+c---+----1----+----2----+----3----+----4----+----5----+----6----+----7
+c      start program
+       program test
+       integer a,b,c
+       a=10
+       b=20
+       c=a+b
+       write(6,*) a,'+',b,'=',c
+c      
+       STOP
+       END
